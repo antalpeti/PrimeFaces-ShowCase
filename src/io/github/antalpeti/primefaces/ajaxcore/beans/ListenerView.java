f3aef89016cdf63d6a8a29f528f2693238ffaf79
Peti <antalpeti@users.noreply.github.com>
2016-02-25 15:48:18 +0100

    Add Ajax Core - Listener example.

---
+++ src/io/github/antalpeti/primefaces/ajaxcore/beans/ListenerView.java
@@ -0,0 +1,20 @@
+package io.github.antalpeti.primefaces.ajaxcore.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="ajaxcoreListenerView")
+public class ListenerView {
+     
+    private String text;
+ 
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+     
+    public void handleKeyEvent() {
+        text = text.toUpperCase();
+    }
+}
