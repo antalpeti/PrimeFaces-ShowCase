e68ac52f45e032b4127cd06931f3f18f06c5534b
Peti <antalpeti@users.noreply.github.com>
2016-03-24 23:45:54 +0100

    Add "Input - Listbox" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/input/beans/SelectOneView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/input/beans/SelectOneView.java
@@ -1,11 +1,25 @@
 package io.github.antalpeti.primefaces.showcase.input.beans;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean(name = "inputSelectOneView")
 public class SelectOneView {
 
   private String option;
+  private Theme theme;
+  private List<Theme> themes;
+
+  @ManagedProperty("#{inputThemeService}")
+  private ThemeService service;
+
+  @PostConstruct
+  public void init() {
+    themes = service.getThemes();
+  }
 
   public String getOption() {
     return option;
@@ -13,5 +27,21 @@ public class SelectOneView {
 
   public void setOption(String option) {
     this.option = option;
+  }
+
+  public Theme getTheme() {
+    return theme;
+  }
+
+  public void setTheme(Theme theme) {
+    this.theme = theme;
+  }
+
+  public List<Theme> getThemes() {
+    return themes;
+  }
+
+  public void setService(ThemeService service) {
+    this.service = service;
   }
 }
