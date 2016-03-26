70c701c3e701124ce408bffacca2b8209021538c
Peti <antalpeti@users.noreply.github.com>
2016-03-26 21:49:54 +0100

    Add "Input - ManyMenu" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/input/beans/SelectManyView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/input/beans/SelectManyView.java
@@ -2,12 +2,32 @@ package io.github.antalpeti.primefaces.showcase.input.beans;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean(name = "inputSelectManyView")
 public class SelectManyView {
 
   private List<String> selectedOptions;
+  private List<Theme> selectedThemes;
+  private List<Theme> themes;
+
+  @ManagedProperty("#{inputThemeService}")
+  private ThemeService service;
+
+  @PostConstruct
+  public void init() {
+    themes = service.getThemes();
+  }
+
+  public List<Theme> getThemes() {
+    return themes;
+  }
+
+  public void setService(ThemeService service) {
+    this.service = service;
+  }
 
   public List<String> getSelectedOptions() {
     return selectedOptions;
@@ -15,5 +35,13 @@ public class SelectManyView {
 
   public void setSelectedOptions(List<String> selectedOptions) {
     this.selectedOptions = selectedOptions;
+  }
+
+  public List<Theme> getSelectedThemes() {
+    return selectedThemes;
+  }
+
+  public void setSelectedThemes(List<Theme> selectedThemes) {
+    this.selectedThemes = selectedThemes;
   }
 }
