ea19e0f9e429d0209e981a219f6b3afbb981a658
Peti <antalpeti@users.noreply.github.com>
2016-04-16 23:16:30 +0200

    Add "Data - DataScroller" example. Remove extra space.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/data/DataScrollerView.java
@@ -0,0 +1,37 @@
+package io.github.antalpeti.primefaces.showcase.data;
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "dataDataScrollerView")
+@ViewScoped
+public class DataScrollerView implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private List<Car> cars;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars = service.createCars(100);
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
+}
