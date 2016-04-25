39f602d4db187385d0ded84cc9cc7a5229ca7188
Peti <antalpeti@users.noreply.github.com>
2016-04-26 00:38:09 +0200

    Refactor. Rename bean.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/BasicView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/BasicView.java
@@ -11,7 +11,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
-@ManagedBean(name = "dataDtBasicView")
+@ManagedBean(name = "datatableDtBasicView")
 @ViewScoped
 public class BasicView implements Serializable {
 
