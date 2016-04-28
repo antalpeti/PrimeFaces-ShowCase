3186124db717d186460f9a33c7b3589cbde776ac
Peti <antalpeti@users.noreply.github.com>
2016-04-28 11:35:47 +0200

    Add "Data - DataTable - Filter" example.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/FilterView.java
@@ -0,0 +1,70 @@
+package io.github.antalpeti.primefaces.showcase.view.data.datatable;
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+ 
+@ManagedBean(name="datatableDtFilterView")
+@ViewScoped
+public class FilterView implements Serializable {
+     
+	private static final long serialVersionUID = 1L;
+
+	private List<Car> cars;
+     
+    private List<Car> filteredCars;
+     
+    @ManagedProperty("#{serviceCarService}")
+    private CarService service;
+ 
+    @PostConstruct
+    public void init() {
+        cars = service.createCars(10);
+    }
+     
+    @SuppressWarnings("unchecked")
+	public boolean filterByPrice(Object value, Object filter, Locale locale) {
+        String filterText = (filter == null) ? null : filter.toString().trim();
+        if(filterText == null||filterText.equals("")) {
+            return true;
+        }
+         
+        if(value == null) {
+            return false;
+        }
+         
+        return ((Comparable<Integer>) value).compareTo(Integer.valueOf(filterText)) > 0;
+    }
+     
+    public List<String> getBrands() {
+        return service.getBrands();
+    }
+     
+    public List<String> getColors() {
+        return service.getColors();
+    }
+     
+    public List<Car> getCars() {
+        return cars;
+    }
+ 
+    public List<Car> getFilteredCars() {
+        return filteredCars;
+    }
+ 
+    public void setFilteredCars(List<Car> filteredCars) {
+        this.filteredCars = filteredCars;
+    }
+ 
+    public void setService(CarService service) {
+        this.service = service;
+    }
+}
