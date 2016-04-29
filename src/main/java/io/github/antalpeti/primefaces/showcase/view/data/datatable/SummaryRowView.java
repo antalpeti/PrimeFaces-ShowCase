33e8ed50358ba6b7f212bacb714de9004b3a2514
Peti <antalpeti@users.noreply.github.com>
2016-04-29 12:24:55 +0200

    Add "Data - DataTable - SummaryRow" example.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/SummaryRowView.java
@@ -0,0 +1,39 @@
+package io.github.antalpeti.primefaces.showcase.view.data.datatable;
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+ 
+@ManagedBean(name="datatableDtSummaryRowView")
+public class SummaryRowView implements Serializable {
+     
+	private static final long serialVersionUID = 1L;
+
+	private List<Car> cars;
+     
+    @ManagedProperty("#{serviceCarService}")
+    private CarService service;
+ 
+    @PostConstruct
+    public void init() {
+        cars = service.createCars(50);
+    }
+     
+    public List<Car> getCars() {
+        return cars;
+    }
+ 
+    public void setService(CarService service) {
+        this.service = service;
+    }
+     
+    public int getRandomPrice() {
+        return (int) (Math.random() * 100000);
+    }
+}
