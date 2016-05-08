fabad844784f6934a4e63da6729b67c6aaad640e
Peti <antalpeti@users.noreply.github.com>
2016-05-08 17:17:20 +0200

    Add "Data - DataTable - Sticky" example.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/StickyView.java
@@ -0,0 +1,35 @@
+package io.github.antalpeti.primefaces.showcase.view.data.datatable;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "datatableDtStickyView")
+public class StickyView implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private List<Car> cars;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars = service.createCars(100);
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
+}
