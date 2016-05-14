991829c3e468fd9fa2db77cbe525f1aa48f7db1a
Peti <antalpeti@users.noreply.github.com>
2016-05-14 14:38:34 +0200

    Refactor: rename bean from datatableDtEditView to datatableEditView. Reformat code.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/EditView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/EditView.java
@@ -1,8 +1,5 @@
 package io.github.antalpeti.primefaces.showcase.view.data.datatable;
 
-import io.github.antalpeti.primefaces.showcase.domain.Car;
-import io.github.antalpeti.primefaces.showcase.service.CarService;
-
 import java.io.Serializable;
 import java.util.List;
 
@@ -15,62 +12,66 @@ import javax.faces.context.FacesContext;
 
 import org.primefaces.event.CellEditEvent;
 import org.primefaces.event.RowEditEvent;
- 
-@ManagedBean(name="datatableDtEditView")
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "datatableEditView")
 @ViewScoped
 public class EditView implements Serializable {
-     
-	private static final long serialVersionUID = 1L;
 
-	private List<Car> cars1;
-    private List<Car> cars2;
-         
-    @ManagedProperty("#{serviceCarService}")
-    private CarService service;
-     
-    @PostConstruct
-    public void init() {
-        cars1 = service.createCars(10);
-        cars2 = service.createCars(10);
+  private static final long serialVersionUID = 1L;
+
+  private List<Car> cars1;
+  private List<Car> cars2;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars1 = service.createCars(10);
+    cars2 = service.createCars(10);
+  }
+
+  public List<Car> getCars1() {
+    return cars1;
+  }
+
+  public List<Car> getCars2() {
+    return cars2;
+  }
+
+  public List<String> getBrands() {
+    return service.getBrands();
+  }
+
+  public List<String> getColors() {
+    return service.getColors();
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
+
+  public void onRowEdit(RowEditEvent event) {
+    FacesMessage msg = new FacesMessage("Car Edited", ((Car) event.getObject()).getId());
+    FacesContext.getCurrentInstance().addMessage(null, msg);
+  }
+
+  public void onRowCancel(RowEditEvent event) {
+    FacesMessage msg = new FacesMessage("Edit Cancelled", ((Car) event.getObject()).getId());
+    FacesContext.getCurrentInstance().addMessage(null, msg);
+  }
+
+  public void onCellEdit(CellEditEvent event) {
+    Object oldValue = event.getOldValue();
+    Object newValue = event.getNewValue();
+
+    if (newValue != null && !newValue.equals(oldValue)) {
+      FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed",
+          "Old: " + oldValue + ", New:" + newValue);
+      FacesContext.getCurrentInstance().addMessage(null, msg);
     }
- 
-    public List<Car> getCars1() {
-        return cars1;
-    }
- 
-    public List<Car> getCars2() {
-        return cars2;
-    }
-     
-    public List<String> getBrands() {
-        return service.getBrands();
-    }
-     
-    public List<String> getColors() {
-        return service.getColors();
-    }
- 
-    public void setService(CarService service) {
-        this.service = service;
-    }
-     
-    public void onRowEdit(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Car Edited", ((Car) event.getObject()).getId());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-     
-    public void onRowCancel(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Car) event.getObject()).getId());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-     
-    public void onCellEdit(CellEditEvent event) {
-        Object oldValue = event.getOldValue();
-        Object newValue = event.getNewValue();
-         
-        if(newValue != null && !newValue.equals(oldValue)) {
-            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
-            FacesContext.getCurrentInstance().addMessage(null, msg);
-        }
-    }
+  }
 }
