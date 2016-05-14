19da8bd735ffdc13c1bb9faa493ce6fda21104a1
Peti <antalpeti@users.noreply.github.com>
2016-05-14 14:41:05 +0200

    Refactor: rename bean from datatableDtFilterView to datatableFilterView. Reformat code.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/FilterView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/FilterView.java
@@ -1,8 +1,5 @@
 package io.github.antalpeti.primefaces.showcase.view.data.datatable;
 
-import io.github.antalpeti.primefaces.showcase.domain.Car;
-import io.github.antalpeti.primefaces.showcase.service.CarService;
-
 import java.io.Serializable;
 import java.util.List;
 import java.util.Locale;
@@ -11,60 +8,63 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
- 
-@ManagedBean(name="datatableDtFilterView")
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "datatableFilterView")
 @ViewScoped
 public class FilterView implements Serializable {
-     
-	private static final long serialVersionUID = 1L;
 
-	private List<Car> cars;
-     
-    private List<Car> filteredCars;
-     
-    @ManagedProperty("#{serviceCarService}")
-    private CarService service;
- 
-    @PostConstruct
-    public void init() {
-        cars = service.createCars(10);
+  private static final long serialVersionUID = 1L;
+
+  private List<Car> cars;
+
+  private List<Car> filteredCars;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars = service.createCars(10);
+  }
+
+  @SuppressWarnings("unchecked")
+  public boolean filterByPrice(Object value, Object filter, Locale locale) {
+    String filterText = filter == null ? null : filter.toString().trim();
+    if (filterText == null || filterText.equals("")) {
+      return true;
     }
-     
-    @SuppressWarnings("unchecked")
-	public boolean filterByPrice(Object value, Object filter, Locale locale) {
-        String filterText = (filter == null) ? null : filter.toString().trim();
-        if(filterText == null||filterText.equals("")) {
-            return true;
-        }
-         
-        if(value == null) {
-            return false;
-        }
-         
-        return ((Comparable<Integer>) value).compareTo(Integer.valueOf(filterText)) > 0;
+
+    if (value == null) {
+      return false;
     }
-     
-    public List<String> getBrands() {
-        return service.getBrands();
-    }
-     
-    public List<String> getColors() {
-        return service.getColors();
-    }
-     
-    public List<Car> getCars() {
-        return cars;
-    }
- 
-    public List<Car> getFilteredCars() {
-        return filteredCars;
-    }
- 
-    public void setFilteredCars(List<Car> filteredCars) {
-        this.filteredCars = filteredCars;
-    }
- 
-    public void setService(CarService service) {
-        this.service = service;
-    }
+
+    return ((Comparable<Integer>) value).compareTo(Integer.valueOf(filterText)) > 0;
+  }
+
+  public List<String> getBrands() {
+    return service.getBrands();
+  }
+
+  public List<String> getColors() {
+    return service.getColors();
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public List<Car> getFilteredCars() {
+    return filteredCars;
+  }
+
+  public void setFilteredCars(List<Car> filteredCars) {
+    this.filteredCars = filteredCars;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
 }
