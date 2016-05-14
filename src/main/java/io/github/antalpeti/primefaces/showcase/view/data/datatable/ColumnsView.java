3ff8c784b3ac10fdee43fabb15f451c957a2fd0b
Peti <antalpeti@users.noreply.github.com>
2016-05-14 14:34:05 +0200

    Refactor: rename bean from datatableDtColumnsView to datatableColumnsView. Reformat code.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/ColumnsView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/ColumnsView.java
@@ -1,8 +1,5 @@
 package io.github.antalpeti.primefaces.showcase.view.data.datatable;
 
-import io.github.antalpeti.primefaces.showcase.domain.Car;
-import io.github.antalpeti.primefaces.showcase.service.CarService;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,101 +11,105 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
- 
-@ManagedBean(name="datatableDtColumnsView")
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "datatableColumnsView")
 @ViewScoped
 public class ColumnsView implements Serializable {
-     
-	private static final long serialVersionUID = 1L;
 
-	private final static List<String> VALID_COLUMN_KEYS = Arrays.asList("id", "brand", "year", "color", "price");
-     
-    private String columnTemplate = "id brand year";
-     
-    private List<ColumnModel> columns;
-     
-    private List<Car> cars;
-     
-    private List<Car> filteredCars;
-     
-    @ManagedProperty("#{serviceCarService}")
-    private CarService service;
- 
-    @PostConstruct
-    public void init() {
-        cars = service.createCars(10);
-         
-        createDynamicColumns();
+  private static final long serialVersionUID = 1L;
+
+  private final static List<String> VALID_COLUMN_KEYS =
+      Arrays.asList("id", "brand", "year", "color", "price");
+
+  private String columnTemplate = "id brand year";
+
+  private List<ColumnModel> columns;
+
+  private List<Car> cars;
+
+  private List<Car> filteredCars;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars = service.createCars(10);
+
+    createDynamicColumns();
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public List<Car> getFilteredCars() {
+    return filteredCars;
+  }
+
+  public void setFilteredCars(List<Car> filteredCars) {
+    this.filteredCars = filteredCars;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
+
+  public String getColumnTemplate() {
+    return columnTemplate;
+  }
+
+  public void setColumnTemplate(String columnTemplate) {
+    this.columnTemplate = columnTemplate;
+  }
+
+  public List<ColumnModel> getColumns() {
+    return columns;
+  }
+
+  private void createDynamicColumns() {
+    String[] columnKeys = columnTemplate.split(" ");
+    columns = new ArrayList<ColumnModel>();
+
+    for (String columnKey : columnKeys) {
+      String key = columnKey.trim();
+
+      if (VALID_COLUMN_KEYS.contains(key)) {
+        columns.add(new ColumnModel(columnKey.toUpperCase(), columnKey));
+      }
     }
-     
-    public List<Car> getCars() {
-        return cars;
+  }
+
+  public void updateColumns() {
+    // reset table state
+    UIComponent table = FacesContext.getCurrentInstance().getViewRoot().findComponent(":form:cars");
+    table.setValueExpression("sortBy", null);
+
+    // update columns
+    createDynamicColumns();
+  }
+
+  static public class ColumnModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String header;
+    private String property;
+
+    public ColumnModel(String header, String property) {
+      this.header = header;
+      this.property = property;
     }
- 
-    public List<Car> getFilteredCars() {
-        return filteredCars;
+
+    public String getHeader() {
+      return header;
     }
- 
-    public void setFilteredCars(List<Car> filteredCars) {
-        this.filteredCars = filteredCars;
+
+    public String getProperty() {
+      return property;
     }
- 
-    public void setService(CarService service) {
-        this.service = service;
-    }
- 
-    public String getColumnTemplate() {
-        return columnTemplate;
-    }
- 
-    public void setColumnTemplate(String columnTemplate) {
-        this.columnTemplate = columnTemplate;
-    }
- 
-    public List<ColumnModel> getColumns() {
-        return columns;
-    }
- 
-    private void createDynamicColumns() {
-        String[] columnKeys = columnTemplate.split(" ");
-        columns = new ArrayList<ColumnModel>();   
-         
-        for(String columnKey : columnKeys) {
-            String key = columnKey.trim();
-             
-            if(VALID_COLUMN_KEYS.contains(key)) {
-                columns.add(new ColumnModel(columnKey.toUpperCase(), columnKey));
-            }
-        }
-    }
-     
-    public void updateColumns() {
-        //reset table state
-        UIComponent table = FacesContext.getCurrentInstance().getViewRoot().findComponent(":form:cars");
-        table.setValueExpression("sortBy", null);
-         
-        //update columns
-        createDynamicColumns();
-    }
-     
-    static public class ColumnModel implements Serializable {
- 
-		private static final long serialVersionUID = 1L;
-		
-		private String header;
-        private String property;
- 
-        public ColumnModel(String header, String property) {
-            this.header = header;
-            this.property = property;
-        }
- 
-        public String getHeader() {
-            return header;
-        }
- 
-        public String getProperty() {
-            return property;
-        }
-    }
+  }
 }
