773739c313b12e02f6aebe6bc96ee3fd50f19b3e
Peti <antalpeti@users.noreply.github.com>
2016-05-15 21:36:12 +0200

    Refactor: rename bean from datatableDtPaginatorView to datatablePaginatorView. Reformat code.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/PaginatorView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/PaginatorView.java
@@ -1,8 +1,5 @@
 package io.github.antalpeti.primefaces.showcase.view.data.datatable;
 
-import io.github.antalpeti.primefaces.showcase.domain.Car;
-import io.github.antalpeti.primefaces.showcase.service.CarService;
-
 import java.io.Serializable;
 import java.util.List;
 
@@ -10,28 +7,31 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
- 
-@ManagedBean(name="datatableDtPaginatorView")
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "datatablePaginatorView")
 @ViewScoped
 public class PaginatorView implements Serializable {
-     
-	private static final long serialVersionUID = 1L;
 
-	private List<Car> cars;
-     
-    @ManagedProperty("#{serviceCarService}")
-    private CarService service;
- 
-    @PostConstruct
-    public void init() {
-        cars = service.createCars(50);
-    }
-     
-    public List<Car> getCars() {
-        return cars;
-    }
- 
-    public void setService(CarService service) {
-        this.service = service;
-    }
+  private static final long serialVersionUID = 1L;
+
+  private List<Car> cars;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars = service.createCars(50);
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
 }
