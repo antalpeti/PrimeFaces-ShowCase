6dd7097c6254bce9a827ea53e923ae900b15cce4
Peti <antalpeti@users.noreply.github.com>
2016-05-16 22:00:51 +0200

    Refactor: rename bean from datatableDtSubTableView to datatableSubTableView. Reformat code.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/SubTableView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/SubTableView.java
@@ -1,42 +1,42 @@
 package io.github.antalpeti.primefaces.showcase.view.data.datatable;
 
-import io.github.antalpeti.primefaces.showcase.domain.Stats;
-import io.github.antalpeti.primefaces.showcase.domain.Team;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
- 
-@ManagedBean(name="datatableDtSubTableView")
+
+import io.github.antalpeti.primefaces.showcase.domain.Stats;
+import io.github.antalpeti.primefaces.showcase.domain.Team;
+
+@ManagedBean(name = "datatableSubTableView")
 public class SubTableView {
-     
-    private List<Team> teams;
- 
-    @PostConstruct
-    public void init() {
-        teams = new ArrayList<Team>();
-        Team lakers = new Team("Los Angeles Lakers");
-        lakers.getStats().add(new Stats("2005-2006", 50, 32));
-        lakers.getStats().add(new Stats("2006-2007", 44, 38));
-        lakers.getStats().add(new Stats("2007-2008", 40, 42));
-        lakers.getStats().add(new Stats("2008-2009", 45, 37));
-        lakers.getStats().add(new Stats("2009-2010", 48, 34));
-        lakers.getStats().add(new Stats("2010-2011", 42, 42));
-        teams.add(lakers);
-         
-        Team celtics = new Team("Boston Celtics");
-        celtics.getStats().add(new Stats("2005-2006", 46, 36));
-        celtics.getStats().add(new Stats("2006-2007", 50, 32));
-        celtics.getStats().add(new Stats("2007-2008", 41, 41));
-        celtics.getStats().add(new Stats("2008-2009", 45, 37));
-        celtics.getStats().add(new Stats("2009-2010", 38, 44));
-        celtics.getStats().add(new Stats("2010-2011", 35, 47));
-        teams.add(celtics);
-    }
-     
-    public List<Team> getTeams() {
-        return teams;
-    }
+
+  private List<Team> teams;
+
+  @PostConstruct
+  public void init() {
+    teams = new ArrayList<Team>();
+    Team lakers = new Team("Los Angeles Lakers");
+    lakers.getStats().add(new Stats("2005-2006", 50, 32));
+    lakers.getStats().add(new Stats("2006-2007", 44, 38));
+    lakers.getStats().add(new Stats("2007-2008", 40, 42));
+    lakers.getStats().add(new Stats("2008-2009", 45, 37));
+    lakers.getStats().add(new Stats("2009-2010", 48, 34));
+    lakers.getStats().add(new Stats("2010-2011", 42, 42));
+    teams.add(lakers);
+
+    Team celtics = new Team("Boston Celtics");
+    celtics.getStats().add(new Stats("2005-2006", 46, 36));
+    celtics.getStats().add(new Stats("2006-2007", 50, 32));
+    celtics.getStats().add(new Stats("2007-2008", 41, 41));
+    celtics.getStats().add(new Stats("2008-2009", 45, 37));
+    celtics.getStats().add(new Stats("2009-2010", 38, 44));
+    celtics.getStats().add(new Stats("2010-2011", 35, 47));
+    teams.add(celtics);
+  }
+
+  public List<Team> getTeams() {
+    return teams;
+  }
 }
