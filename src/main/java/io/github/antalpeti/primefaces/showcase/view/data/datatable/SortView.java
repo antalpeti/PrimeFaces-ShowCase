b42889517b7b52affe6abef0e6ef9c332fbcd8dd
Peti <antalpeti@users.noreply.github.com>
2016-05-16 21:52:40 +0200

    Refactor: rename bean from datatableDtSortView to datatableSortView. Reformat code.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/SortView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/datatable/SortView.java
@@ -1,8 +1,5 @@
 package io.github.antalpeti.primefaces.showcase.view.data.datatable;
 
-import io.github.antalpeti.primefaces.showcase.domain.Car;
-import io.github.antalpeti.primefaces.showcase.service.CarService;
-
 import java.io.Serializable;
 import java.util.List;
 
@@ -10,40 +7,43 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
- 
-@ManagedBean(name="datatableDtSortView")
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "datatableSortView")
 @ViewScoped
 public class SortView implements Serializable {
-     
-	private static final long serialVersionUID = 1L;
 
-	private List<Car> cars1;
-    private List<Car> cars2;
-    private List<Car> cars3;
-     
-    @ManagedProperty("#{serviceCarService}")
-    private CarService service;
- 
-    @PostConstruct
-    public void init() {
-        cars1 = service.createCars(10);
-        cars2 = service.createCars(10);
-        cars3 = service.createCars(50);
-    }
- 
-    public List<Car> getCars1() {
-        return cars1;
-    }
- 
-    public List<Car> getCars2() {
-        return cars2;
-    }
-     
-    public List<Car> getCars3() {
-        return cars3;
-    }
-    
-    public void setService(CarService service) {
-        this.service = service;
-    }
+  private static final long serialVersionUID = 1L;
+
+  private List<Car> cars1;
+  private List<Car> cars2;
+  private List<Car> cars3;
+
+  @ManagedProperty("#{serviceCarService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars1 = service.createCars(10);
+    cars2 = service.createCars(10);
+    cars3 = service.createCars(50);
+  }
+
+  public List<Car> getCars1() {
+    return cars1;
+  }
+
+  public List<Car> getCars2() {
+    return cars2;
+  }
+
+  public List<Car> getCars3() {
+    return cars3;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
 }
