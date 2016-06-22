895e8919b34297df0444a236222416ce1918cfa2
Peti <antalpeti@users.noreply.github.com>
2016-06-22 23:34:05 +0200

    Add "Data - Tree - Icon" example.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/data/tree/IconView.java
@@ -0,0 +1,31 @@
+package io.github.antalpeti.primefaces.showcase.view.data.tree;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import org.primefaces.model.TreeNode;
+
+import io.github.antalpeti.primefaces.showcase.service.DocumentService;
+
+@ManagedBean(name = "treeIconView")
+public class IconView {
+
+  private TreeNode root;
+
+  @ManagedProperty("#{documentService}")
+  private DocumentService service;
+
+  @PostConstruct
+  public void init() {
+    root = service.createDocuments();
+  }
+
+  public void setService(DocumentService service) {
+    this.service = service;
+  }
+
+  public TreeNode getRoot() {
+    return root;
+  }
+}
