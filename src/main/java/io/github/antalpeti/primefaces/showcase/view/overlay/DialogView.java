f5af493649c28de4d91b3f479da6d16dd1866be3
Peti <antalpeti@users.noreply.github.com>
2016-07-30 16:37:27 +0200

    Add "Overlay - Dialog - Events" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/overlay/DialogView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/overlay/DialogView.java
@@ -4,11 +4,23 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.event.CloseEvent;
+import org.primefaces.event.MoveEvent;
+
 @ManagedBean(name = "dialogView")
 public class DialogView {
 
   public void destroyWorld() {
     addMessage("System Error", "Please try again later.");
+  }
+
+  public void handleClose(CloseEvent event) {
+    addMessage(event.getComponent().getId() + " closed", "So you don't like nature?");
+  }
+
+  public void handleMove(MoveEvent event) {
+    addMessage(event.getComponent().getId() + " moved",
+        "Left: " + event.getLeft() + ", Top: " + event.getTop());
   }
 
   public void addMessage(String summary, String detail) {
