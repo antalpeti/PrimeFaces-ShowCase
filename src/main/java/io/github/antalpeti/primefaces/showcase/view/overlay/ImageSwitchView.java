38bed475f9d991a7be59cd2e4740915bf6123dff
Peti <antalpeti@users.noreply.github.com>
2016-08-07 00:23:32 +0200

    Add "Overlay - OverlayPanel" example.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/overlay/ImageSwitchView.java
@@ -0,0 +1,27 @@
+package io.github.antalpeti.primefaces.showcase.view.overlay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ImageSwitchView {
+
+  private List<String> images;
+
+  @PostConstruct
+  public void init() {
+    images = new ArrayList<String>();
+
+    images.add("nature1.jpg");
+    images.add("nature2.jpg");
+    images.add("nature3.jpg");
+    images.add("nature4.jpg");
+  }
+
+  public List<String> getImages() {
+    return images;
+  }
+}
---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/overlay/MovieView.java
@@ -0,0 +1,43 @@
+package io.github.antalpeti.primefaces.showcase.view.overlay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+import io.github.antalpeti.primefaces.showcase.domain.Movie;
+
+@ManagedBean
+public class MovieView {
+
+  private List<Movie> movieList;
+
+  public List<Movie> getMovieList() {
+    return movieList;
+  }
+
+  @PostConstruct
+  public void init() {
+    movieList = new ArrayList<Movie>();
+
+    movieList.add(
+        new Movie("The Lord of the Rings: The Two Towers", "Peter Jackson", "Fantasy, Epic", 179));
+    movieList.add(new Movie("The Amazing Spider-Man 2", "Marc Webb", "Action", 142));
+    movieList.add(new Movie("Iron Man 3", "Shane Black", "Action", 109));
+    movieList.add(new Movie("Thor: The Dark World", "Alan Taylor", "Action, Fantasy", 112));
+    movieList.add(new Movie("Avatar", "James Cameron", "Science Fiction", 160));
+    movieList.add(new Movie("The Lord of the Rings: The Fellowship of the Ring", "Peter Jackson",
+        "Fantasy, Epic", 165));
+    movieList.add(new Movie("Divergent", "Neil Burger", "Action", 140));
+    movieList
+        .add(new Movie("The Hobbit: The Desolation of Smaug", "Peter Jackson", "Fantasy", 161));
+    movieList.add(new Movie("Rio 2", "Carlos Saldanha", "Comedy", 101));
+    movieList.add(new Movie("Captain America: The Winter Soldier", "Joe Russo", "Action", 136));
+    movieList.add(new Movie("Fast Five", "Justin Lin", "Action", 132));
+    movieList.add(new Movie("The Lord of the Rings: The Return of the King", "Peter Jackson",
+        "Fantasy, Epic", 200));
+
+  }
+
+}
