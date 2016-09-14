cf30b89ffdda7ae9cac44427fa486c5bd909c2aa
Peti <antalpeti@users.noreply.github.com>
2016-09-14 21:10:51 +0200

    Add "Charts - Interactive" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.event.ItemSelectEvent;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
@@ -17,6 +20,7 @@ import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 import org.primefaces.model.chart.MeterGaugeChartModel;
+import org.primefaces.model.chart.PieChartModel;
 
 @ManagedBean(name = "chartsChartView")
 public class ChartView implements Serializable {
@@ -32,6 +36,8 @@ public class ChartView implements Serializable {
     createLineModels();
     createMeterGaugeModels();
     createAnimatedModels();
+    createBarModels();
+    createPieModels();
   }
 
   public LineChartModel getAreaModel() {
@@ -298,5 +304,60 @@ public class ChartView implements Serializable {
     model.addSeries(girls);
 
     return model;
+  }
+
+  private BarChartModel barModel;
+  private PieChartModel pieModel1;
+
+
+  public BarChartModel getBarModel() {
+    return barModel;
+  }
+
+  public PieChartModel getPieModel1() {
+    return pieModel1;
+  }
+
+  private void createBarModels() {
+    createBarModel();
+  }
+
+  private void createBarModel() {
+    barModel = initBarModel();
+
+    barModel.setTitle("Bar Chart");
+    barModel.setLegendPosition("ne");
+
+    Axis xAxis = barModel.getAxis(AxisType.X);
+    xAxis.setLabel("Gender");
+
+    Axis yAxis = barModel.getAxis(AxisType.Y);
+    yAxis.setLabel("Births");
+    yAxis.setMin(0);
+    yAxis.setMax(200);
+  }
+
+  private void createPieModels() {
+    createPieModel1();
+  }
+
+  private void createPieModel1() {
+    pieModel1 = new PieChartModel();
+
+    pieModel1.set("Brand 1", 540);
+    pieModel1.set("Brand 2", 325);
+    pieModel1.set("Brand 3", 702);
+    pieModel1.set("Brand 4", 421);
+
+    pieModel1.setTitle("Simple Pie");
+    pieModel1.setLegendPosition("w");
+  }
+
+  public void itemSelect(ItemSelectEvent event) {
+    FacesMessage msg =
+        new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected", "Item Index: "
+            + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
+
+    FacesContext.getCurrentInstance().addMessage(null, msg);
   }
 }
