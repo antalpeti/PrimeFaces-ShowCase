d180e72e29e07f2642dbf3702f7b8c1a42fc1285
Peti <antalpeti@users.noreply.github.com>
2016-09-20 22:13:13 +0200

    Add "Charts - Date" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
@@ -19,6 +19,7 @@ import org.primefaces.model.chart.BubbleChartSeries;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.DateAxis;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 import org.primefaces.model.chart.MeterGaugeChartModel;
@@ -41,6 +42,7 @@ public class ChartView implements Serializable {
     createBarModels();
     createPieModels();
     createCombinedModel();
+    createDateModel();
   }
 
   public LineChartModel getAreaModel() {
@@ -401,5 +403,48 @@ public class ChartView implements Serializable {
     Axis yAxis = combinedModel.getAxis(AxisType.Y);
     yAxis.setMin(0);
     yAxis.setMax(200);
+  }
+
+  private LineChartModel dateModel;
+
+
+  public LineChartModel getDateModel() {
+    return dateModel;
+  }
+
+  private void createDateModel() {
+    dateModel = new LineChartModel();
+    LineChartSeries series1 = new LineChartSeries();
+    series1.setLabel("Series 1");
+
+    series1.set("2014-01-01", 51);
+    series1.set("2014-01-06", 22);
+    series1.set("2014-01-12", 65);
+    series1.set("2014-01-18", 74);
+    series1.set("2014-01-24", 24);
+    series1.set("2014-01-30", 51);
+
+    LineChartSeries series2 = new LineChartSeries();
+    series2.setLabel("Series 2");
+
+    series2.set("2014-01-01", 32);
+    series2.set("2014-01-06", 73);
+    series2.set("2014-01-12", 24);
+    series2.set("2014-01-18", 12);
+    series2.set("2014-01-24", 74);
+    series2.set("2014-01-30", 62);
+
+    dateModel.addSeries(series1);
+    dateModel.addSeries(series2);
+
+    dateModel.setTitle("Zoom for Details");
+    dateModel.setZoom(true);
+    dateModel.getAxis(AxisType.Y).setLabel("Values");
+    DateAxis axis = new DateAxis("Dates");
+    axis.setTickAngle(-50);
+    axis.setMax("2014-02-01");
+    axis.setTickFormat("%b %#d, %y");
+
+    dateModel.getAxes().put(AxisType.X, axis);
   }
 }
