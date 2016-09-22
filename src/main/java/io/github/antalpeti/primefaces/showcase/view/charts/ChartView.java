2028666022a2d35565b195d75499287ec75ae64c
Peti <antalpeti@users.noreply.github.com>
2016-09-22 20:54:15 +0200

    Add "Charts - Bar" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
@@ -20,6 +20,7 @@ import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.DateAxis;
+import org.primefaces.model.chart.HorizontalBarChartModel;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.LineChartSeries;
 import org.primefaces.model.chart.MeterGaugeChartModel;
@@ -325,6 +326,7 @@ public class ChartView implements Serializable {
 
   private void createBarModels() {
     createBarModel();
+    createHorizontalBarModel();
   }
 
   private void createBarModel() {
@@ -446,5 +448,46 @@ public class ChartView implements Serializable {
     axis.setTickFormat("%b %#d, %y");
 
     dateModel.getAxes().put(AxisType.X, axis);
+  }
+
+  private HorizontalBarChartModel horizontalBarModel;
+
+  public HorizontalBarChartModel getHorizontalBarModel() {
+    return horizontalBarModel;
+  }
+
+  private void createHorizontalBarModel() {
+    horizontalBarModel = new HorizontalBarChartModel();
+
+    ChartSeries boys = new ChartSeries();
+    boys.setLabel("Boys");
+    boys.set("2004", 50);
+    boys.set("2005", 96);
+    boys.set("2006", 44);
+    boys.set("2007", 55);
+    boys.set("2008", 25);
+
+    ChartSeries girls = new ChartSeries();
+    girls.setLabel("Girls");
+    girls.set("2004", 52);
+    girls.set("2005", 60);
+    girls.set("2006", 82);
+    girls.set("2007", 35);
+    girls.set("2008", 120);
+
+    horizontalBarModel.addSeries(boys);
+    horizontalBarModel.addSeries(girls);
+
+    horizontalBarModel.setTitle("Horizontal and Stacked");
+    horizontalBarModel.setLegendPosition("e");
+    horizontalBarModel.setStacked(true);
+
+    Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
+    xAxis.setLabel("Births");
+    xAxis.setMin(0);
+    xAxis.setMax(200);
+
+    Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
+    yAxis.setLabel("Gender");
   }
 }
