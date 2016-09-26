a8ed62405634990ed236ff97efa4c9bf33728356
Peti <antalpeti@users.noreply.github.com>
2016-09-26 23:18:46 +0200

    Add "Charts - Pie" example.

--- src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/charts/ChartView.java
@@ -350,6 +350,7 @@ public class ChartView implements Serializable {
 
   private void createPieModels() {
     createPieModel1();
+    createPieModel2();
   }
 
   private void createPieModel1() {
@@ -545,5 +546,26 @@ public class ChartView implements Serializable {
     model.addCircle(circle3);
 
     return model;
+  }
+
+  private PieChartModel pieModel2;
+
+  public PieChartModel getPieModel2() {
+    return pieModel2;
+  }
+
+  private void createPieModel2() {
+    pieModel2 = new PieChartModel();
+
+    pieModel2.set("Brand 1", 540);
+    pieModel2.set("Brand 2", 325);
+    pieModel2.set("Brand 3", 702);
+    pieModel2.set("Brand 4", 421);
+
+    pieModel2.setTitle("Custom Pie");
+    pieModel2.setLegendPosition("e");
+    pieModel2.setFill(false);
+    pieModel2.setShowDataLabels(true);
+    pieModel2.setDiameter(150);
   }
 }
