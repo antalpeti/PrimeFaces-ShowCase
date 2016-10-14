dc06aa7f93137ba93dc934c6ad7a52d72af3acb2
Peti <antalpeti@users.noreply.github.com>
2016-10-14 22:19:46 +0200

    Add "Multimedia - ContentFlow" example.

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/multimedia/ImagesView.java
@@ -0,0 +1,25 @@
+package io.github.antalpeti.primefaces.showcase.view.multimedia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "multimediaImagesView")
+public class ImagesView {
+
+  private List<String> images;
+
+  @PostConstruct
+  public void init() {
+    images = new ArrayList<String>();
+    for (int i = 1; i <= 12; i++) {
+      images.add("nature" + i + ".jpg");
+    }
+  }
+
+  public List<String> getImages() {
+    return images;
+  }
+}
