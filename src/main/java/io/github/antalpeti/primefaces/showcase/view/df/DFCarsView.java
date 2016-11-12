c4ed3f6dfc7571a70da5d075a7ed48bd8846b0fe
Peti <antalpeti@users.noreply.github.com>
2016-11-12 21:45:25 +0100

    15: Add "Dialog Framework - Basic" example.
    Task-Url: https://github.com/antalpeti/primefaces-showcase/issues/15

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/df/DFCarsView.java
@@ -0,0 +1,35 @@
+package io.github.antalpeti.primefaces.showcase.view.df;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import io.github.antalpeti.primefaces.showcase.domain.Car;
+import io.github.antalpeti.primefaces.showcase.service.CarService;
+
+@ManagedBean(name = "dfCarsView")
+@ViewScoped
+public class DFCarsView implements Serializable {
+
+  private List<Car> cars;
+
+  @ManagedProperty("#{carService}")
+  private CarService service;
+
+  @PostConstruct
+  public void init() {
+    cars = service.createCars(9);
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void setService(CarService service) {
+    this.service = service;
+  }
+}
