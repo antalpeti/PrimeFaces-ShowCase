3fe58f052b265e49820f854f6fff8c73b2af1146
Peti <antalpeti@users.noreply.github.com>
2016-12-09 22:19:44 +0100

    34: Add "Misc - ImportConstants" example.
    Task-Url: https://github.com/antalpeti/primefaces-showcase/issues/34

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/util/Constants.java
@@ -0,0 +1,10 @@
+package io.github.antalpeti.primefaces.showcase.view.util;
+
+public class Constants {
+
+  public static final String DOWNLOAD_COOKIE = "primefaces.download";
+
+  public final static String LIBRARY = "primefaces";
+
+  public final static String PUSH_PATH = "/primepush";
+}
