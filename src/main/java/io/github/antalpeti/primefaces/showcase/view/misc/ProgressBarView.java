9dc5451c18b9223297773f12a812f3bae9f72e0c
Peti <antalpeti@users.noreply.github.com>
2016-12-18 09:31:48 +0100

    40: Add "Misc - ProgressBar" example.
    Task-Url: https://github.com/antalpeti/primefaces-showcase/issues/40

---
+++ src/main/java/io/github/antalpeti/primefaces/showcase/view/misc/ProgressBarView.java
@@ -0,0 +1,40 @@
+package io.github.antalpeti.primefaces.showcase.view.misc;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "miscProgressBarView")
+@ViewScoped
+public class ProgressBarView implements Serializable {
+
+  private Integer progress;
+
+  public Integer getProgress() {
+    if (progress == null) {
+      progress = 0;
+    } else {
+      progress = progress + (int) (Math.random() * 35);
+
+      if (progress > 100)
+        progress = 100;
+    }
+
+    return progress;
+  }
+
+  public void setProgress(Integer progress) {
+    this.progress = progress;
+  }
+
+  public void onComplete() {
+    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Progress Completed"));
+  }
+
+  public void cancel() {
+    progress = null;
+  }
+}
